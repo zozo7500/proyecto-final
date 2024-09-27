e301ec3835c1b07a40405e09b3d0429311e1ac2c
zoooe <130598786+zozo7500@users.noreply.github.com>
2024-09-27 20:41:18 -0300

    Delete mijava.js

--- mijava.js
+++
@@ -1,7 +0,0 @@
-botonsi = document.querySelector("#boton1")
-
-botonsi.addEventListener("click", () => {
-    alert("Estas de acuerdo")
-    document.querySelector("body").style.backgroundColor="green"
-    document.querySelector("#title").style.color="white"
-})
