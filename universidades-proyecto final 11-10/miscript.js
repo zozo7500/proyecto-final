5127f6db78496ef04bd4c1d62a5c8c1d15c0951c
zoooe <130598786+zozo7500@users.noreply.github.com>
2024-10-11 23:35:58 -0300

    Add files via upload

---
+++ universidades-proyecto final 11-10/miscript.js
@@ -0,0 +1,492 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja1');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Bioquimica' : 'Letras';
+    }, 3000); // Cambia el texto cada 2 segundos
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja2');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Bibliotecologia' : 'Matematica';
+    }, 3000); // Cambia el texto cada 2 segundos
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja3');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Historia' : 'Farmacia';
+    }, 3000); // Cambia el texto cada 2 segundos
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja4');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Quimica' : 'Geografia';
+    }, 3000); // Cambia el texto cada 2 segundos
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja5');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Fisica' : 'Psicologia';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja6');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Economia' : 'Educación fisica';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja7');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Administración' : 'Geofisica';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja8');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Astronomia' : 'Agronomia';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja9');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Artes Visuales' : 'Música';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja10');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Arquitectura' : 'Ingenieria en Informatica';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja11');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Fisica' : 'Psicologia';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja12');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Economia' : 'Educación fisica';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja13');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Diseño en Comunicación Visual' : 'Diseño Multimedial';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja14');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Diseño Industrial' : 'Composición';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja15');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Dirección Coral' : 'Dirección Orquestal';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja16');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? ' Educación Musical' : 'Guitarra';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja17');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Música Popular' : 'Piano';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja18');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ceramica' : 'Dibujo';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja19');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Escenografia' : 'Escultura';
+    }, 3000);
+});
+/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja20');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Grabado' : 'Muralismo';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja21');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Pintura' : ' Dirección de Fotografía';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja21');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Guión' : 'Realización';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja22');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Teoría y Crítica' : 'Historia de las Artes';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja23');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ingeniería Forestal' : ' Meteorología y Ciencias de la Atmósfera ';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja24');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Contador Público' : 'Turismo';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja25');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ciencia de Datos en Organizaciones' : 'Óptica Ocular y Optometría';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja26');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Química y Tecnología Ambiental' : 'Abogacía';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja27');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Escribania' : 'Enfermería Universitaria';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja28');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Nutrición' : 'Obstreticia';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja29');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Medicina' : 'Tec Prácticas Cardiológicas';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja30');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Antropologia' : 'Botánica';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja31');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ecología' : 'Paleontología';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja32');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Zoologia' : 'Geologia';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja33');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Geoquimica' : 'Medicina Veterinaria';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja34');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Microbiología' : ' Cs. de la Educación';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja35');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Filosofia' : 'Sociología';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja36');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? ' Ciencias Biológicas' : ' Sistemas';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja37');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ciencia de Datos en Organizaciones' : 'Computación';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja38');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Analista Programador Universitario' : 'Ingeniería Aeroespacial';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja39');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ingeniería en Agrimensura' : 'Ingeniería Civil';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja40');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ingeniería Electrónica' : 'Ingeniería Electromecánica';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja41');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ingeniería Hidráulica' : 'Ingeniería en Materiales';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja42');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Ingeniería Química' : 'Ingeniería Industrial';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja43');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? 'Odontología' : 'Comunicación Social';
+    }, 3000);
+});/*----------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const caja = document.getElementById('miCaja44');
+    let cambiar = false;
+
+    setInterval(function() {
+        cambiar = !cambiar;
+        caja.classList.toggle('cambiar', cambiar);
+        caja.textContent = cambiar ? ' Trabajo Social' : 'Fonoaudiologia';
+    }, 3000);
+});/*----------------------------------*/
+    ScrollReveal().reveal('.pag1', { delay: 200 });
+    ScrollReveal().reveal('.content', { delay: 200 });
+    ScrollReveal().reveal('.aurora', { delay: 200 });
+    ScrollReveal().reveal('.subtitle', { delay: 200 });
+    ScrollReveal().reveal('.pag2', { delay: 200 });
+    ScrollReveal().reveal('.introduccion', { delay: 200 });
+    ScrollReveal().reveal('.imagenes', { delay: 200 });
+    ScrollReveal().reveal('.info_pag', { delay: 200 });
+    ScrollReveal().reveal('.pag4-1', { delay: 200 });
+    ScrollReveal().reveal('.pag3', { delay: 200 });
+    ScrollReveal().reveal('.card', { delay: 200 });
+    ScrollReveal().reveal('.contenedor1', { delay: 200 });
+    ScrollReveal().reveal('.pag4', { delay: 200 });
+    ScrollReveal().reveal('.contenedor2', { delay: 200 });
+    ScrollReveal().reveal('.info', { delay: 200 });
+    ScrollReveal().reveal('.ubicacion', { delay: 200 });
+    ScrollReveal().reveal('.oferta', { delay: 200 });
+
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById('fecha').textContent = today.toLocaleDateString('es-ES', options);
+
+
